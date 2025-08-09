87ca8c47ee7adc3c113469c5eab0622a161807e2
kmedya-dev <koustavamedya.2000@gmail.com>
2025-08-09 20:02:04 +0530

    Fix: Resolve pyjnius dependency conflict and update requirements

--- p4a-recipes/pyjnius/__init__.py
+++ p4a-recipes/pyjnius/__init__.py
@@ -1,22 +1,18 @@
 from pythonforandroid.recipes.pyjnius import PyjniusRecipe
-from pythonforandroid.toolchain import Recipe
 
-class PyjniusFixRecipe(PyjniusRecipe):
+class PatchedPyjniusRecipe(PyjniusRecipe):
     """
     This is a custom recipe to apply a patch to pyjnius.
     """
     # We use the same version as the original recipe we want to patch
     version = '1.6.1'
 
-    # This is the name of our custom recipe
-    name = 'pyjnius_fix'
+    # This is the name of our custom recipe, which is the same as the original one
+    name = 'pyjnius'
 
     # This is the list of patches to apply to the source code
     # p4a will automatically apply these patches.
     patches = ['pyjnius_long_fix.patch']
 
-    # We need to tell p4a that this recipe conflicts with the original pyjnius recipe
-    conflicts = ['pyjnius']
-
 # This is the entry point for p4a to find our recipe
-recipe = PyjniusFixRecipe()
+recipe = PatchedPyjniusRecipe()
