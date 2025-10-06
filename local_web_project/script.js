b4989007b88d16e9f9ad22134fdd48b2d454fa2c
kmedya <koustavamedya.2000@gmail.com>
2025-10-06 15:15:14 +0530

    create workflow

--- local_web_project/script.js
+++ local_web_project/script.js
@@ -1,2 +1,6 @@
 console.log("Script loaded!");
 document.getElementById('message').textContent = "Content updated by JavaScript!";
+
+document.getElementById('myButton').addEventListener('click', () => {
+    alert('Button clicked!');
+});
