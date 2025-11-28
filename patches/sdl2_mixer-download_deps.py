5edf93754d7eccb34f5fb2fd767d854691f69ba2
kmedya <koustavamedya.2000@gmail.com>
2025-11-28 05:39:05 +0530

    Trigger CI build (test)

--- patches/sdl2_mixer-download_deps.py
+++ patches/sdl2_mixer-download_deps.py
@@ -1,4 +1,3 @@
-#!/usr/bin/env python3
 import os
 import sys
 from droidbuilder.utils.command_executor import run_shell_command
@@ -9,23 +8,15 @@ package_source_path = os.getcwd()
 
 download_script_path = os.path.join(package_source_path, "external", "download.sh")
 
-logger.info(f"  - Making {download_script_path} executable.")
-chmod_result = run_shell_command(
-    command=["chmod", "+x", download_script_path],
-    description=f"Making {os.path.basename(download_script_path)} executable",
-    cwd=package_source_path
-)
-
-if chmod_result['returncode'] != 0:
-    logger.error(f"  - Failed to make {os.path.basename(download_script_path)} executable. Aborting patch.")
-    sys.exit(1)
-
 logger.info(f"  - Executing {download_script_path}.")
 run_result = run_shell_command(
-    command=[download_script_path],
+    command=["bash",download_script_path],
     description=f"Running {os.path.basename(download_script_path)}",
     cwd=package_source_path
 )
+
+if run_result('stdout'):
+    logger.debug(run_result['stdout'])
 
 if run_result['returncode'] != 0:
     logger.error(f"  - {os.path.basename(download_script_path)} failed (Exit Code: {run_result['returncode']}). Aborting patch.")
@@ -33,6 +24,6 @@ if run_result['returncode'] != 0:
         logger.error(f"    Stdout:\n{run_result['stdout']}")
     if run_result.get('stderr'):
         logger.error(f"    Stderr:\n{run_result['stderr']}")
-    sys.exit(1)
+    return False
 
 logger.success(f"  - Successfully executed {os.path.basename(download_script_path)}.")
